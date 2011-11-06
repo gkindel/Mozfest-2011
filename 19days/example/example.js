a04910fa951d9c2989cbcc562dec692e57261e75
Greg Kindel <gkindel@ramp.com>
2011-11-06 12:14:27 +0000

    upload tpb movie

---
+++ 19days/example/example.js
@@ -0,0 +1,36 @@
+
+$( function () {
+    var fullwidth = false;
+
+
+    $('#video').click( function () {
+        toggleOverlay();
+    });
+
+    $('.feed    ').mouseenter( function (){
+        example.pause();
+    });
+
+    $('.feed').mouseleave( function (){
+        example.play();
+
+    });
+});
+
+
+function toggleOverlay( bool, now ){
+    fullwidth = ! ( bool == null ? fullwidth : bool );
+    var cw = $('.container').width();
+    var tw = $('#feeddiv').width();
+    var vw = fullwidth ? cw : cw - tw;
+
+    if( now )
+        $('#video').width(vw);
+    else
+        $('#video').animate({width:  vw}, 1000);
+
+    $('.feed').stop().animate({
+        'width': (fullwidth ? 0 : 300),
+        'opacity': (fullwidth ? 0 : 1)
+    }, 1000);
+}
---
+++ 19days/timefeed.js
@@ -0,0 +1,149 @@
+
+(function () {
+    var $ = jQuery;
+
+    var defaults = {
+        hightlight: true
+    };
+
+    var TimeFeed = function ( target, options ) {
+
+        if( TimeFeed.instances[target] )
+            return TimeFeed.instances[target];
+
+        if( !( this instanceof TimeFeed))
+            return new TimeFeed(target, options)
+
+        this.config = $.extend({}, defaults, options);
+        this.items = [];
+        this.el = target;
+        this.options = options;
+        this.visible = true;
+
+        TimeFeed.instances[target] = this;
+    };
+
+    TimeFeed.instances = {};
+
+    TimeFeed.getFav = function  ( url ) {
+        var matches = url.match(/(https?:\/\/[^\/]*)/);
+        return matches[1] + "/favicon.ico";
+    };
+
+    TimeFeed.prototype =  {
+        add : function (options) {
+            var el = $("<div class='item'></item>");
+            el.append("<div class='title'></item>");
+            el.append("<div class='time'><span class='start'>N</span>s</item>");
+            el.append("<img class='ico' />");
+            el.append("<div class='body'></item>");
+            el.append("<a class='link'></a>");
+            el.css('opacity', 0);
+            el.hide();
+            $(this.el).prepend(el);
+            options._el = el;
+            this.items.push(options);
+            return el;
+        },
+
+        render : function (options, player){
+            var self= this;
+            var el = $(options._el);
+            el.find('.title').text( options.title );
+            el.find('.body').text( options.body );
+            el.find('.start').text( options.start );
+            el.find('img').attr('src',  options.img || TimeFeed.getFav(options.href) );
+            el.find('.link').attr('target',  "_blank");
+            el.find('.link').attr('href',  options.href)
+                .text(options.href)
+                .click( function () {
+                    player.pause();
+            });
+            el.click( function () {
+                if( player.paused() )
+                    player.pause(options.start);
+                else
+                    player.play(options.start);
+
+            });
+
+            el.mouseenter( function (){
+                self.mouseEnter();
+            });
+
+            el.mouseleave( function (){
+                self.mouseLeave();
+            });
+
+            el.find('a').click( function (e) {
+                e.stopPropagation();
+            });
+
+            el.show();
+            this.focus(options);
+        },
+
+        player : function (popcorn) {
+            if( !(popcorn === undefined) )
+                this._player = popcorn;
+            return this._player;
+        },
+
+        mouseEnter : function (){
+//            this._player.pause();
+        },
+
+        mouseLeave : function (){
+//            this._player.play();
+        },
+
+        focus : function (options) {
+            var el = $(options._el).animate({'opacity': 1}, 1000, function () {
+            });
+        },
+
+        blur : function (options) {
+            if(! this.config.highlight )
+                return;
+            var el = $(options._el).animate({'opacity': .5}, 1000, function () {
+                el.css('opacity', '');
+            });
+        },
+
+        toggle : function (bool) {
+            var show;
+            if( bool == null)
+                show = "toggle";
+            else if( bool )
+                show = 1;
+            else
+                show = 0;
+            $('#feeddiv').animate({'opacity': show});
+        }
+
+    };
+    window.timefeed = TimeFeed;
+})();
+
+
+(function () {
+    if( ! Popcorn )
+        return;
+
+    Popcorn.plugin( "timefeed" , {
+        _setup : function (options) {
+            timefeed(options.target).player(this);
+            timefeed(options.target).add(options);
+        },
+        start : function (event, options) {
+            timefeed(options.target).render(options, this);
+        },
+        end : function (event, options) {
+            timefeed(options.target).blur(options);
+        },
+        teardown : function (options) {
+
+        }
+    });
+
+})();
